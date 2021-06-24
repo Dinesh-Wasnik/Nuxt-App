63df386c1ca8e826943a6874b79ca04a76da9f58
Dinesh Wasnik <dinesh.wasnik@i-serve.com.my>
2021-06-24 10:42:43 +0800

    Second sample test

--- test/Hero.spec.js
+++ test/Hero.spec.js
@@ -1,9 +1,17 @@
 import { mount } from '@vue/test-utils'
 import Hero from '@/components/Hero.vue'
+import Reviews from '@/components/Reviews.vue'
 
 describe('Hero', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(Hero)
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
+
+describe('Reviews', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(Reviews)
     expect(wrapper.vm).toBeTruthy()
   })
 })
